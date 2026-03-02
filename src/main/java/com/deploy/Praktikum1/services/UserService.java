8c2b95e390b87b5dd55f7115c906e8865e83e06f
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-03-02 14:53:30 +0700

    Add methods for user management in UserService interface

--- src/main/java/com/deploy/Praktikum1/services/UserService.java
+++ src/main/java/com/deploy/Praktikum1/services/UserService.java
@@ -1,4 +1,15 @@
 package com.deploy.Praktikum1.services;
 
+import com.deploy.Praktikum1.model.dto.UserDto;
+import com.deploy.Praktikum1.model.dto.UserAddRequest;
+
+import java.util.List;
+
 public interface UserService {
+    UserDto AddUser(UserAddRequest Request);
+    List<UserDto> getAllUser();
+    UserDto getUserById(String id);
+     UserDto updateUser(String id, UserAddRequest Request);
+     void deleteUser(String id);
+
 }
